9660bfffeecc110afafb21445d08e0d4fe6ed732
a-y-u <ayushialokjain@gmail.com>
2018-11-08 22:25:12 +0530

    Completed the module2

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
+
 import { Response } from '@angular/http';
+
 import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
